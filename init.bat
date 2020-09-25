7d1361d519d17ae77871399a55846e29ab9330ef
Monish Singhal <programminggeekishere@gmail.com>
2020-09-25 20:35:15 +0530

    Update init.bat

--- init.bat
+++ init.bat
@@ -2,12 +2,8 @@
 @echo off
 
 mkdir config
-echo "confgured"
 COPY  default_config.yml config\config.yml
-echo "copied"
 copy /b config\token.txt +,,
-echo "copied 2.0"
 mkdir data
-echo "made directory"
 sqlite3 data\data.db
 
