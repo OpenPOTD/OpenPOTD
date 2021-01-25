9b613bda5045689dd3afae46bf1da2227558f649
IcosahedralDice <etomnz@gmail.com>
2021-01-25 21:53:12 +1300

    Add rate command

--- cogs/ratings.py
+++ cogs/ratings.py
@@ -33,6 +33,90 @@ class Ratings(commands.Cog):
         self.bot = bot
         self.waiting_for = {}
 
+    @commands.command(brief="Rate two problems you've been given.")
+    async def rate(self, ctx, *, rating):
+        if ctx.author.id not in self.waiting_for:
+            await ctx.send("You haven't got any problems to rate! "
+                           "Get some with `%rate_difficulty` or `%rate_coolness`.")
+            return
+
+        if rating not in ('1', '2', 'n', 'd'):
+            await ctx.send("That's not a valid rating! Use `d` if you want to abandon ratings.")
+            return
+
+        choices: ChoiceInformation = self.waiting_for[ctx.author.id]
+        cursor = self.bot.db.cursor()
+
+        # Get number of ratings for each problem and calculate elo K value
+        cursor.execute('SELECT COUNT() FROM rating_choices WHERE problem_1_id = ? OR problem_2_id = ? AND type = ?',
+                       (choices.p1.id, choices.p1.id, choices.type))
+        number_1 = cursor.fetchall()[0][0]
+        elo_k_1 = 10 + 100 * 10 ** (number_1 / -20)
+        cursor.execute('SELECT COUNT() FROM rating_choices WHERE problem_1_id = ? OR problem_2_id = ? AND type = ?',
+                       (choices.p2.id, choices.p2.id, choices.type))
+        number_2 = cursor.fetchall()[0][0]
+        elo_k_2 = 10 + 100 * 10 ** (number_2 / -20)
+
+        # Log rating
+        cursor.execute('INSERT INTO rating_choices (problem_1_id, problem_2_id, choice, type) VALUES (?, ?, ?, ?)',
+                       (choices.p1.id, choices.p2.id, rating, choices.type))
+        self.bot.db.commit()
+
+        # If it doesn't care, void
+        if rating == 'd':
+            await ctx.send("Thank you for your rating.")
+            return
+
+        # Calculate elo stuffs
+        s_value_1 = 1 if rating == '1' else 0.5 if rating == 'n' else 0
+        s_value_2 = 1 - s_value_1
+
+        if choices.type == 'DIFF':
+
+            expected_1 = 1 / (1 + 10 ** ((choices.p2.difficulty_rating - choices.p1.difficulty_rating) / 400))
+            expected_2 = 1 / (1 + 10 ** ((choices.p1.difficulty_rating - choices.p2.difficulty_rating) / 400))
+
+            new_1 = choices.p1.difficulty_rating + elo_k_1 * (s_value_1 - expected_1)
+            new_2 = choices.p2.difficulty_rating + elo_k_2 * (s_value_2 - expected_2)
+
+            # Update the database
+            cursor.executemany('UPDATE problems SET difficulty_rating = ? WHERE problems.id = ?',
+                               [
+                                   (new_1, choices.p1.id),
+                                   (new_2, choices.p2.id)
+                               ])
+            self.bot.db.commit()
+
+            old_1 = choices.p1.difficulty_rating
+            old_2 = choices.p2.difficulty_rating
+
+        elif choices.type == 'COOL':
+            expected_1 = 1 / (1 + 10 ** ((choices.p2.coolness_rating - choices.p1.coolness_rating) / 400))
+            expected_2 = 1 / (1 + 10 ** ((choices.p1.coolness_rating - choices.p2.coolness_rating) / 400))
+
+            new_1 = choices.p1.coolness_rating + elo_k_1 * (s_value_1 - expected_1)
+            new_2 = choices.p2.coolness_rating + elo_k_2 * (s_value_2 - expected_2)
+
+            # Update the database
+            cursor.executemany('UPDATE problems SET coolness_rating = ? WHERE problems.id = ?',
+                               [
+                                   (new_1, choices.p1.id),
+                                   (new_2, choices.p2.id)
+                               ])
+            self.bot.db.commit()
+
+            old_1 = choices.p1.coolness_rating
+            old_2 = choices.p2.coolness_rating
+
+        else:
+            self.bot.logger.error(f'[RATING] No such type {choices.type}')
+            return
+
+        await ctx.send(f"Thank you for your rating. The ratings for the two problems were {old_1:.2f} and {old_2:.2f} "
+                       f"respectively and they have changed to {new_1:.2f} and {new_2:.2f} respectively.")
+
+        del self.waiting_for[ctx.author.id]
+
     @commands.command(brief="Grab two problems you've solved to compare the difficulty of. ")
     async def rate_difficulty(self, ctx):
         if ctx.author.id in self.waiting_for:
